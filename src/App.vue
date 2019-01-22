2346680a0713e503110ba81bca77bc367f78ae39
d-velev <dimitar.velev@quanterall.com>
2019-01-22 13:55:10 +0200

    Initial vue project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <pending-submissions></pending-submissions>
+    <ae-button face="round">button content</ae-button>
+  </div>
+</template>
+<script>
+
+import PendingSubmissions from './components/PendingSubmissions'
+import { AeButton } from '@aeternity/aepp-components'
+
+export default {
+  name: 'app',
+  components: {
+    AeButton,
+    PendingSubmissions
+  }
+}
+</script>
+<style>
+</style>
---
+++ src/components/PendingSubmissions.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+<script>
+import { AeMain } from '@aeternity/aepp-components'
+
+export default {
+  name: 'app',
+  components: {
+    AeMain
+  }
+}
+</script>
+<style>
+</style>
