4f3d2e021d0cbb02a8c47400631d890d832a07c9
cheezus1 <bobinikolov33@gmail.com>
2019-01-25 08:49:06 +0200

    Retrieving contract storage

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <pending-submissions></pending-submissions>
-    <ae-button face="round">button content</ae-button>
   </div>
 </template>
 <script>
 import PendingSubmissions from "./components/PendingSubmissions";
 import { AeButton } from "@aeternity/aepp-components";
+import ContractState from "./util/contract_state";
 
 export default {
   name: "app",
   components: {
     AeButton,
     PendingSubmissions
+  },
+  data () {
+    return { data: ContractState.get() }
   }
 };
 </script>
--- src/components/PendingSubmissions.vue
+++ src/components/PendingSubmissions.vue
@@ -1,4 +1,27 @@
 <template>
+  <ae-card>
+  <template slot="avatar">
+    <ae-identicon address="ak_QY8VNEkhj7omMUjAvfVBq2NjTDy895LBYbk7qVxQo1qT8VqfE" />
+    <ae-input-plain placeholder="Account name" value="My account" />
+  </template>
+  <ae-icon name="more" size="20px" slot="header" />
+  <ae-text face="uppercase-xs" weight=600 style="margin: 0">Normal Secured</ae-text>
+  <ae-address value="ak_QY8VNEkhj7omMUjAvfVBq2NjTDy895LBYbk7qVxQo1qT8VqfE" length="medium" gap=0 />
+  <ae-toolbar align="right" slot="footer">
+    <ae-button face="toolbar">
+      <ae-icon name="eye" />
+      Details
+    </ae-button>
+    <ae-button face="toolbar">
+      <ae-icon name="copy" />
+      Copy
+    </ae-button>
+    <ae-button face="toolbar">
+      <ae-icon name="share" />
+      Share
+    </ae-button>
+  </ae-toolbar>
+</ae-card>
 </template>
 <script>
 import { AeMain } from "@aeternity/aepp-components";
