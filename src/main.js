00e3e3ee4ca94882b3800231622ed5282d62858d
d-velev <dimitar.velev@quanterall.com>
2019-01-28 15:46:44 +0200

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Components from "@aeternity/aepp-components";
+import BootstrapVue from "bootstrap-vue";
 import "@aeternity/aepp-components/dist/aepp.components.css";
 
+Vue.use(BootstrapVue);
 Vue.use(Components);
 
 new Vue({
